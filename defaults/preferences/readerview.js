aebfdb40fa61a281b77939f41edba77dbeb100ea
Ascrod <32915892+Ascrod@users.noreply.github.com>
2017-12-07 21:00:04 -0500

    Add first_run pref, update node limit default and includeURL default

--- defaults/preferences/readerview.js
+++ defaults/preferences/readerview.js
@@ -1,12 +1,13 @@
+pref("extensions.reader.first_run", true);
 pref("extensions.reader.color_scheme", "light");
 pref("extensions.reader.color_scheme.values", "[\"light\", \"dark\", \"sepia\"]");
 pref("extensions.reader.content_width", 3);
-pref("extensions.reader.errors.includeURLs", false);
+pref("extensions.reader.errors.includeURLs", true);
 pref("extensions.reader.font_size", 5);
 pref("extensions.reader.font_type", "sans-serif");
 pref("extensions.reader.has_used_toolbar", false);
 pref("extensions.reader.line_height", 4);
-pref("extensions.reader.parse-node-limit", 3000);
+pref("extensions.reader.parse-node-limit", 0);
 pref("extensions.reader.parse-on-load.enabled", true);
 pref("extensions.reader.parse-on-load.force-enabled", false);
 pref("extensions.reader.toolbar.vertical", true);
